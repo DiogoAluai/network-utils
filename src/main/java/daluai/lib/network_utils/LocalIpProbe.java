cf18a49cd503a104912d08edee151a96d26272a8
Diogo Aluai <diogoaluai@gmail.com>
2024-08-11 23:28:59 +0100

    Clean up registry api Create registry client and move functionality there. No dependency on spring, so that android app can work with it also.

--- src/main/java/daluai/lib/network_utils/LocalIpProbe.java
+++ src/main/java/daluai/lib/network_utils/LocalIpProbe.java
@@ -1,43 +1,56 @@
 package daluai.lib.network_utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Enumeration;
+import java.util.Objects;
 
 public class LocalIpProbe {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LocalIpProbe.class);
+
+    public record IpResult(NetworkInterface networkInterface, InetAddress inetAddress) {
+    }
 
     /**
      * Gets the IPv4 address of the first active, non-loopback network interface.
      * @return The IPv4 address as a String, or null if no suitable address is found.
      */
     public static String firstActiveIPv4Address() {
+        String firstActiveInetAddress = Objects.requireNonNull(firstActiveIPv4Interface()).inetAddress().getHostAddress();
+        System.out.println("Using IPv4 Address: " + firstActiveInetAddress);
+        return firstActiveInetAddress;
+    }
+
+    public static IpResult firstActiveIPv4Interface() {
         try {
             Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
 
             while (networkInterfaces.hasMoreElements()) {
                 NetworkInterface networkInterface = networkInterfaces.nextElement();
 
-                if (networkInterface.isUp() && !networkInterface.isLoopback()) {
+                if (networkInterface.isUp() && !networkInterface.isLoopback() && !networkInterface.getName().startsWith("docker")) {
                     Enumeration<InetAddress> inetAddresses = networkInterface.getInetAddresses();
-                    
+
                     while (inetAddresses.hasMoreElements()) {
                         InetAddress inetAddress = inetAddresses.nextElement();
-                        
+
                         // Check for IPv4 address
                         if (inetAddress.getAddress().length == 4) {
                             System.out.println("Using network interface: " + networkInterface.getName());
-                            System.out.println("IPv4 Address: " + inetAddress.getHostAddress());
-                            return inetAddress.getHostAddress();
+                            return new IpResult(networkInterface, inetAddress);
                         }
                     }
                 }
             }
         } catch (SocketException e) {
-            e.printStackTrace();
+            LOG.error("Failed to fetch first active ip", e);
         }
-
-        return null; // No suitable address found
+        return null;
     }
 }
 
--- src/test/java/daluai/lib/network_utils/DemoApplicationTests.java
+++ src/test/java/daluai/lib/network_utils/DemoApplicationTests.java
@@ -2,8 +2,10 @@ package daluai.lib.network_utils;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
+import java.net.InetAddress;
+import java.util.Objects;
+
+import static org.junit.Assert.*;
 
 public class DemoApplicationTests {
 
@@ -18,12 +20,18 @@ public class DemoApplicationTests {
 	}
 
 	/**
-	 * Interface depends on computer, so this test might fail for diferent computers.
+	 * Interface depends on computer, so this test might fail for different computers.
 	 * Test that it locates wlo1.
 	 * @author daluai
 	 */
 	@Test
 	public void localProbe() {
-		LocalIpProbe.firstActiveIPv4Address()
+		LocalIpProbe.IpResult ipResult = Objects.requireNonNull(LocalIpProbe.firstActiveIPv4Interface());
+
+		String interfaceName = ipResult.networkInterface().getName();
+        assertEquals("wlo1", interfaceName);
+
+		String ipAddress = ipResult.inetAddress().getHostAddress();
+        assertFalse(ipAddress.isEmpty());
 	}
 }
